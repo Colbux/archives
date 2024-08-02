b1003fb9e29fda74f4247296a1445bd778a9e366
eqozqq <112166693+eqozqq@users.noreply.github.com>
2024-08-02 16:23:03 +0300

    Add files via upload

---
+++ toolbox/js.js
@@ -0,0 +1,25 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+function toggleVisible(elId) {
+	var el = document.getElementById(elId);
+	if (el.style.display == "none") {
+		el.style.display = "block";
+	} else if (el.style.display == "block") {
+		el.style.display = "none";
+	} else {
+		el.style.display = (el.className.indexOf("show") == -1 ? "block" : "none");
+	}
+}
+
+
+}
